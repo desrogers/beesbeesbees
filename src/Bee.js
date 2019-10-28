2b92c0099f88b68d2e9c95fa5666ee233c3fab83
Desmond Rogers <desmondrogers2@gmail.com>
2019-10-28 12:04:22 -0700

    Implement pseudoclassical subclasses for bees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
 var Bee = function() {
+  Grub.call(this); // makes bee a sbuclass of superclass Grub
+  this.age = 5;
+  this.color = 'yellow';
+  this.food;
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+  this.color;
+  this.food;
+
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function() { this.honeyPot++ };
+
+HoneyMakerBee.prototype.giveHoney = function() { this.honeyPot-- };
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
